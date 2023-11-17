25f2de677c38ad5ca0bdd7ad5dd03f07d34ce6bd
leebonghun <dlqhdgns613@naver.com>
2023-11-18 00:40:00 +0900

    20231118_2 commit

--- PRC_20231118/PRC20231118_1.java
+++ PRC_20231118/PRC20231118_1.java
@@ -1,6 +1,6 @@
 package PRC_20231118;
 
-public class PRC20231118 {
+public class PRC20231118_1 {
     public static void main(String[] args){
         System.out.println("Hi");
     }
---
+++ PRC_20231118/PRC20231118_2.java
@@ -0,0 +1,21 @@
+package PRC_20231118;
+
+public class PRC20231118_2 {
+    public static void main(String[] args) {
+        int num = (int)(Math.random()*6)+1;
+
+        if(num==1){
+            System.out.println("주사위 1");
+        }else if(num==2){
+            System.out.println("주사위 2");
+        }else if(num==3){
+            System.out.println("주사위 3");
+        }else if(num==4){
+            System.out.println("주사위 4");
+        }else if(num==5){
+            System.out.println("주사위 5");
+        }else{
+            System.out.println("주사위 6");
+        }
+    }
+}
