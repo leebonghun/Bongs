82c4b72afe6d36fa8affd96b587b0342d2e4c4d7
leebonghun <dlqhdgns613@naver.com>
2023-11-18 00:53:45 +0900

    third commit

--- PRC_20231118/PRC20231118_2.java
+++ PRC_20231118/PRC20231118_2.java
@@ -4,7 +4,7 @@ public class PRC20231118_2 {
     public static void main(String[] args) {
         int num = (int) (Math.random() * 6) + 1;
 
-        if (num == 1) {
+        /*if (num == 1) {
             System.out.println("주사위 1");
         } else if (num == 2) {
             System.out.println("주사위 2");
@@ -16,6 +16,26 @@ public class PRC20231118_2 {
             System.out.println("주사위 5");
         } else {
             System.out.println("주사위 6");
+        }*/
+        switch(num){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                System.out.println("4");
+                break;
+            case 5:
+                System.out.println("5");
+                break;
+            default:
+                System.out.println("6");
+                break;
         }
 
         int lotto[] = new int[6];
@@ -31,5 +51,7 @@ public class PRC20231118_2 {
             }
             System.out.print(lotto[i] + " ");
         }
+
+
     }
 }
