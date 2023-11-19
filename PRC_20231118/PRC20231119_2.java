acf20105d2357185070fa12b64d856a4e5f96751
leebonghun <dlqhdgns613@naver.com>
2023-11-19 12:53:17 +0900

    20231119_2

---
+++ PRC_20231118/PRC20231119_2.java
@@ -0,0 +1,12 @@
+package PRC_20231118;
+
+public class PRC20231119_2 {
+    public static void main(String[] args) {
+        for(int i= 1; i<=10; i++){
+            if(i%2 != 0){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
